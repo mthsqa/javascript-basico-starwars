a4d76d90e4d1ec8b6cada06feb52a96714045848
Matheus Silva <matheus_jooe@hotmail.com>
2024-10-22 16:40:19 -0300

    Javascript basico

---
+++ js/basic.js
@@ -0,0 +1,53 @@
+console.log('Olá, javascript!')
+
+var userName = 'Jedi'
+
+document.getElementById('user-name').innerHTML = userName
+
+
+// Variaveis //
+// var nome ='Mestre Yoda'
+// var idade = 100
+// var jedi = true
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+// console.log(typeof jedi)
+
+// Operadores Matemáticos //
+
+var n1 = 5
+var n2 = 2.5
+
+// + para somar
+// - para subtrair
+// * para multiplicar
+// / para divisão
+
+console.log(typeof n1)
+console.log(typeof n2)
+
+var total = n1 / n2 
+console.log(total)
+
+// Funções //
+
+// function soma(n1, n2) {
+// console.log(n1 + n2)
+// }
+
+// soma(5, 100)
+
+// function boasVindas(Jedi) {
+// alert(nome +', seja bem-vindo(a)')
+
+// Constantes //
+
+const nome = 'Darth Vader'
+console.log(nome)
+
+nome = 'Mestre Yoda'
+console.log(nome)
+
+
+
